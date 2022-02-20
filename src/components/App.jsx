51dee47ec3c867b907482332c2d9a347b6585a82
anton <anton.kantry@ya.ru>
2022-02-20 14:38:23 +0500

    add main paige draft

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Login.jsx';
 import NoMatch from './NoMatch.jsx';
+import Main from './Main.jsx';
 
 const App = () => (
     <BrowserRouter>
         <Routes>
+            <Route path="/" element={<Main />} />
             <Route path="login" element={<Login />} />
             <Route path="*" element={<NoMatch />} />
         </Routes>
