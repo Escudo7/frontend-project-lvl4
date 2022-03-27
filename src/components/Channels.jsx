2ca902b2b676e4b968ba786f83604df4c3e2b4f6
anton <anton.kantry@ya.ru>
2022-03-27 17:16:24 +0500

    change active channel

---
+++ src/components/Channels.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import classNames from 'classnames';
+import { setActiveChannelId } from '../slices/channalsSlice.js';
+
+const Channels = () => {
+  const dispatch = useDispatch();
+  const { channels, activeChannelId } = useSelector((state) => state.channels.value);
+
+  const setActiveChannelHandler = (channelId) => () => {
+    dispatch(setActiveChannelId(channelId));
+  };
+
+  return (
+    <div className="col-4 col-md-2 border-right pt-5 px-0 bg-light">
+      <div className="d-flex justify-content-between mb-2 pl-4 pr-2">
+        <span>Каналы</span>
+        <button type="button" className="p-0 text-primary btn btn-group-vertical">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+        </button>
+      </div>
+      <ul className="nav flex-column nav-pills nav-fill px-2">
+        <li className="nav-item w-100">
+          {channels.map((channel) => {
+            const classes = classNames('w-100', 'rounded-0', 'text-left', 'btn', {
+              'btn-secondary': channel.id === activeChannelId,
+            });
+
+            return (
+              <button type="button" className={classes} key={channel.id} onClick={setActiveChannelHandler(channel.id)}>
+                <span className="me-1">#</span>
+                {channel.name}
+              </button>
+            );
+          })}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Channels;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
-import classNames from 'classnames';
 import {
   Formik,
   Form,
@@ -11,21 +10,7 @@ import {
 import { logOut } from '../slices/authorizedSlice.js';
 import { loadChannels, setActiveChannelId } from '../slices/channalsSlice.js';
 import { loadMessages } from '../slices/messagesSlice.js';
-
-const renderChannels = (channels, activeChannelId) => (
-  channels.map((channel) => {
-    const classes = classNames('w-100', 'rounded-0', 'text-left', 'btn', {
-      'btn-secondary': channel.id === activeChannelId,
-    });
-
-    return (
-      <button type="button" className={classes} key={channel.id}>
-        <span className="me-1">#</span>
-        {channel.name}
-      </button>
-    );
-  })
-);
+import Channels from './Channels.jsx';
 
 const renderMessages = (messages) => (
   <div id="messages-box" className="overflow-auto px-5 mr-3">
@@ -96,22 +81,7 @@ const Main = ({ socket }) => {
   return (
     <div className="container-xxl mx-5 my-4 h-100 overflow-hidden rounded shadow-lg">
       <div className="row h-100 bg-white flex-md-row">
-        <div className="col-4 col-md-2 border-right pt-5 px-0 bg-light">
-          <div className="d-flex justify-content-between mb-2 pl-4 pr-2">
-            <span>Каналы</span>
-            <button type="button" className="p-0 text-primary btn btn-group-vertical">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-              </svg>
-            </button>
-          </div>
-          <ul className="nav flex-column nav-pills nav-fill px-2">
-            <li className="nav-item w-100">
-              {renderChannels(channels, activeChannelId)}
-            </li>
-          </ul>
-        </div>
+        <Channels />
         <div className="col p-0 h-100">
           <div className="d-flex flex-column h-100">
             <div className="bg-light mb-4 p-3 shadow-sm">
