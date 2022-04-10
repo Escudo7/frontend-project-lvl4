ca3f2f248f78e6e57dd3a1aef3bcafec4b11ba2f
anton <anton.kantry@ya.ru>
2022-04-10 23:36:22 +0500

    update code style

--- src/components/modals/RenameChannel.jsx
+++ src/components/modals/RenameChannel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Modal } from 'react-bootstrap';
 import * as Yup from 'yup';
